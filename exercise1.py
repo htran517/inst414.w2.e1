66863903acd9696d5bacc1f3a26d71ce95b9a680
htran517 <htran517@terpmail.umd.edu>
2023-02-01 12:07:05 -0500

    style fix

--- exercise1.py
+++ exercise1.py
@@ -7,5 +7,6 @@ with open('imdb_movies_1985to2022.json', 'r') as f:
         for i in range(len(this_movie['actors'])):
             if this_movie['actors'][i][1] == 'Hugh Jackman':
                 arr.append(this_movie['rating']['avg'])
+                
 print(sum(arr)/len(arr))
         
